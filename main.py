f0e95b8e8ffcb8d0af4aa6e90b9e0ac4508607fc
Muhammad Abdullah <232841637+abdullahee297@users.noreply.github.com>
2026-01-05 10:35:49 +0500

    Hand_model added

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
+BaseOptions = python.BaseOptions
+HandLandmarker = vision.HandLandmarker
+HandLandmarkerOptions = vision.HandLandmarkerOptions
+VisionRunningMode = vision.RunningMode
+
+options = HandLandmarkerOptions(
+    base_options = BaseOptions(model_asset_path = "hand_landmarker.task"),
+    running_mode = VisionRunningMode.IMAGE,
+    num_hands = 1
+)
+
+detector = HandLandmarker.create_from_options(options)
+
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -13,8 +26,22 @@ while True:
     if not success:
         break
     
+    rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    mp_image = mp.Image(mp.ImageFormat.SRGB, rgb)
     
+    result = detector.detect(mp_image)
     
+    if result.hand_landmarks:
+        for hand in result.hand_landmarks:
+            
+            h, w, _ = img.shape
+            lm_list = []
+            
+            for lm in hand:
+                lm_list.append((int(lm.x * w), int(lm.y * h)))
+            
+            for x, y in lm_list:
+                cv2.circle(img, (x, y), 8, (0, 255, 0), cv2.FILLED)
     
     
     
